dd07f3497979b5d7f32ad32476da5108ffc5121e
Pjotr Prins <pjotr.public01@thebird.nl>
2018-07-28 00:05:29 +0000

    Fixes:
    BioD/bio/maf/reader.d(53): Error: cannot implicitly convert expression `this._f.byLine(cast(Flag)true, '\x0a')` of type `ByLineImpl!(char, char)` to `ByLine!(char, char)`
    
    See https://github.com/bioconda/bioconda-recipes/pull/8787#issuecomment-389195848

--- bio/maf/reader.d
+++ bio/maf/reader.d
@@ -1,6 +1,7 @@
 /*
     This file is part of BioD.
     Copyright (C) 2013    Artem Tarasov <lomereiter@gmail.com>
+    Copyright (C) 2018    Pjotr Prins <pjotr.prins@thebird.nl>
 
     Permission is hereby granted, free of charge, to any person obtaining a
     copy of this software and associated documentation files (the "Software"),
@@ -8,10 +9,10 @@
     the rights to use, copy, modify, merge, publish, distribute, sublicense,
     and/or sell copies of the Software, and to permit persons to whom the
     Software is furnished to do so, subject to the following conditions:
-    
+
     The above copyright notice and this permission notice shall be included in
     all copies or substantial portions of the Software.
-    
+
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -50,7 +51,7 @@ struct MafBlockRange {
 
     this(string fn) {
         _f = File(fn);
-        _lines = _f.byLine(KeepTerminator.yes);
+        _lines = cast(LineRange)_f.byLine(KeepTerminator.yes);
         skipHeader();
         popFront();
     }
@@ -85,7 +86,7 @@ struct MafBlockRange {
 
 ///
 class MafReader {
- 
+
     private string _fn;
 
     ///
