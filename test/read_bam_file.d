6b9fa8989b57c1027453171e7e5cef42cdfc528c
Pjotr Prins <pjotr.public01@thebird.nl>
2019-11-23 15:26:52 -0600

    segfault

--- test/read_bam_file.d
+++ test/read_bam_file.d
@@ -44,24 +44,6 @@ void main() {
                            false,     // reads don't contain MD tags
                            155, 158); // specify [start, end) interval
 
-  foreach (column; pileup) {
-    writeln("Reference position: ", column.position);
-    writeln("    Coverage: ", column.coverage);
-    writeln("    Reads:");
-
-    foreach (read; column.reads) {
-      writefln("%30s\t%s\t%.2d\t%s\t%2s/%2s\t%2s/%2s\t%10s\t%s %s",
-               read.name,
-               read.current_base,
-               read.current_base_quality,
-               read.cigar_operation,
-               read.cigar_operation_offset + 1, read.cigar_operation.length,
-               read.query_offset + 1, read.sequence.length,
-               read.cigarString(),
-               read.cigar_before, read.cigar_after);
-    }
-  }
-
   {
     auto fn = buildPath(dirName(__FILE__), "data", "b.sam");
     auto sam = new SamReader(fn);
